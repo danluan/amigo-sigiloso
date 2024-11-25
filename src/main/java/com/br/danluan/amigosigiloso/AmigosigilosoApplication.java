9c2f2f95fb129fb4f97121f36b15fef87b2230e3
danluan <danielluanlourencol@gmail.com>
2024-11-25 18:06:42 -0300

    primeiro commit

---
+++ src/main/java/com/br/danluan/amigosigiloso/AmigosigilosoApplication.java
@@ -0,0 +1,13 @@
+package com.br.danluan.amigosigiloso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosigilosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosigilosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/danluan/amigosigiloso/AmigosigilosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.danluan.amigosigiloso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosigilosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
