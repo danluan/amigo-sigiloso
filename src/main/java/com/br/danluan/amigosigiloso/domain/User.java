dd88477b9cd1c1f5441d604f5b4a916693a79a38
danluan <danielluanlourencol@gmail.com>
2024-12-05 18:16:42 -0300

    feat: user entity e repository

---
+++ src/main/java/com/br/danluan/amigosigiloso/domain/User.java
@@ -0,0 +1,28 @@
+package com.br.danluan.amigosigiloso.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
